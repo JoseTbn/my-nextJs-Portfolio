f3389e1d2762e1a67f1a551ec752a6ad8e41eb9c
JoseTbn <josebatumeni@gmail.com>
2022-01-02 13:00:36 +0100

    smh

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Contact from '../Components/Contact'
-import Navbar from '../Components/Navbar'
+import Contact from './Components/Contact'
+import Navbar from './Components/Navbar'
 
 export default function contact() {
     return (
         < > 
-            <Navbar/>
-            <Contact/>
+           
         </>
     )
 }
